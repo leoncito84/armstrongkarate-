237a21ed3e19a6d8dc8d5129ed4e663a20b56768
leoncito84 <leoncito@live.com>
2016-06-05 23:12:36 +0100

    Add Date and time

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -13,13 +13,19 @@ namespace armstrongkarate_.Models
 
         [Required]
         [Display(Name = "Customer must ID")]
-        public int CustomerID { get; set; } 
+        public int CustomerID { get; set; }
+
+
+        [Required]
+        [Display(Name = "Your Title")]
+        [StringLength(4, ErrorMessage = "Your title must least 4")]
+        public string Tle { get; set; }
 
 
         [Required]
         [Display(Name = "Your surname")]
-        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
-        public string SName { get; set; }
+        [StringLength(50, ErrorMessage = "Surname cannot be longer than 50 characters.")]
+        public string SName { get; set; } 
 
 
 
@@ -27,6 +33,15 @@ namespace armstrongkarate_.Models
         [Display(Name = "Your firstname")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FName { get; set; }
+
+
+  
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+
+
+        public DateTime EnrollmentDate { get; set; }
 
 
     }
