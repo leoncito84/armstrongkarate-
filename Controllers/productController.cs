0b003bd2f5e95999fd858ebe468aa121299b679b
leoncito84 <leoncito@live.com>
2016-06-06 15:24:20 +0100

    Product on controller  for create 'CRUD' on result include = Enrollment Date

--- Controllers/productController.cs
+++ Controllers/productController.cs
@@ -47,7 +47,7 @@ namespace armstrongkarate_.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductID,pName,Descrition,Quantity,price")] product product)
+        public ActionResult Create([Bind(Include = "ProductID,pName,Descrition,Quantity,price,Enrollment Date")] product product)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace armstrongkarate_.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductID,pName,Descrition,Quantity,price")] product product)
+        public ActionResult Edit([Bind(Include = "ProductID,pName,Descrition,Quantity,price,Enrollment Date")] product product)
         {
             if (ModelState.IsValid)
             {
